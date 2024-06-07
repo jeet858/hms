ddb7ccef05e8dce9b9cdfe98794393d4ba56c852
Achinta27 <achinta27sep1999@gmail.com>
2024-06-07 15:30:20 +0530

    available doctor page done

---
+++ src/pages/available-doctor.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DashboardTemplate } from "~/components";
+import AvailableDoctorBoxComponent from "~/components/doctor/AvailableDoctorBoxComponent";
+
+const availabledoctor: React.FunctionComponent = () => {
+  return (
+    <DashboardTemplate active_tile="Doctors">
+      <div className="flex w-full flex-col gap-4 p-4">
+        <div className="flex w-full items-center justify-between">
+          <span className="text-2xl font-semibold">Available Doctor</span>
+          <span className="text-xl font-semibold">+ Add Doctor</span>
+        </div>
+        <div className="">
+          <AvailableDoctorBoxComponent />
+        </div>
+      </div>
+    </DashboardTemplate>
+  );
+};
+
+export default availabledoctor;
