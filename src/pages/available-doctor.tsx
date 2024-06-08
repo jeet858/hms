c90a31fbb6966d797251448132b3905dcdffb544
Achinta27 <achinta27sep1999@gmail.com>
2024-06-08 23:16:08 +0530

    doctor profile done

--- src/pages/available-doctor.tsx
+++ src/pages/available-doctor.tsx
@@ -7,8 +7,12 @@ const availabledoctor: React.FunctionComponent = () => {
     <DashboardTemplate active_tile="Doctors">
       <div className="flex w-full flex-col gap-4 p-4">
         <div className="flex w-full items-center justify-between">
-          <span className="text-2xl font-semibold">Available Doctor</span>
-          <span className="text-xl font-semibold">+ Add Doctor</span>
+          <span className="text-2xl font-semibold text-[#00738C]">
+            Available Doctor
+          </span>
+          <span className="text-xl font-semibold text-[#000000BF]">
+            <span className="text-2xl text-[#FFB800]">+</span> Add Doctor
+          </span>
         </div>
         <div className="">
           <AvailableDoctorBoxComponent />
