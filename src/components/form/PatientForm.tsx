d5e315a5d8a9cbc2494055170cd1d6adf76fb545
jeetm31 <jeetmajumder858@gmail.com>
2024-06-17 11:59:21 +0530

    css fix

--- src/components/form/PatientForm.tsx
+++ src/components/form/PatientForm.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import PersonalInfo from "./patient/PersonalInfo";
 import ContactDetails from "./patient/ContactDetails";
 import Insurance from "./patient/Insurance";
@@ -7,60 +7,95 @@ import AdmissionDetails from "./patient/AdmissionDetails";
 import OtherInfo from "./patient/OtherInfo";
 
 const PatientForm: React.FC = () => {
-  const [form,setForm]=useState<"personalInfo"|"contactInfo"|"insurance"|"attatchment"|"admissionDetails"|"otherInfo">("personalInfo")
-  const renderPatientForm=()=>{
-    switch(form){
+  const [form, setForm] = useState<
+    | "personalInfo"
+    | "contactInfo"
+    | "insurance"
+    | "attatchment"
+    | "admissionDetails"
+    | "otherInfo"
+  >("personalInfo");
+  const renderPatientForm = () => {
+    switch (form) {
       case "personalInfo":
-        return <PersonalInfo/>
+        return <PersonalInfo />;
         break;
       case "contactInfo":
-        return <ContactDetails/>
+        return <ContactDetails />;
         break;
       case "insurance":
-        return <Insurance/>
+        return <Insurance />;
         break;
       case "otherInfo":
-        return <OtherInfo/>
+        return <OtherInfo />;
         break;
       case "attatchment":
-        return <Attatchment/>
+        return <Attatchment />;
         break;
       case "admissionDetails":
-        return <AdmissionDetails/>
+        return <AdmissionDetails />;
         break;
     }
-  }
+  };
   return (
-    <div className="min-h-full w-full flex flex-col border ">
-      <div className="flex font-sansation font-bold h-[2rem] flex-row items-center justify-evenly">
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("personalInfo")}>
+    <div className="flex min-h-full w-full flex-col border ">
+      <div className="font-sansation flex h-fit flex-row items-center justify-evenly pb-6 pt-4 font-bold">
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("personalInfo")}
+        >
           Personal Information{" "}
-          <div className={`h-[10%] w-full ${form=="personalInfo"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "personalInfo" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("contactInfo")}>
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("contactInfo")}
+        >
           Contact Information
-          <div className={`h-[10%] w-full ${form=="contactInfo"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "contactInfo" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("insurance")}>
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("insurance")}
+        >
           Insurance
-          <div className={`h-[10%] w-full ${form=="insurance"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "insurance" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("otherInfo")}>
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("otherInfo")}
+        >
           Other Information
-          <div className={`h-[10%] w-full ${form=="otherInfo"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "otherInfo" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("attatchment")}>
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("attatchment")}
+        >
           Attachment
-          <div className={`h-[10%] w-full ${form=="attatchment"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "attatchment" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
-        <span className="flex h-full cursor-pointer flex-col justify-between" onClick={()=>setForm("admissionDetails")}>
+        <span
+          className="flex h-full cursor-pointer flex-col justify-between"
+          onClick={() => setForm("admissionDetails")}
+        >
           Admission Details
-          <div className={`h-[10%] w-full ${form=="admissionDetails"? "block":"hidden"} bg-yellow-400`}></div>
+          <div
+            className={`h-[10%] w-full ${form == "admissionDetails" ? "block" : "hidden"} bg-yellow-400`}
+          ></div>
         </span>
       </div>
-      <div className="w-full grow">
-        {renderPatientForm()}
-      </div>
+      <div className="h-full min-h-full w-full grow">{renderPatientForm()}</div>
     </div>
   );
 };
