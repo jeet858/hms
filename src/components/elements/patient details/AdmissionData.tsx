f23a2c0bac643ad850b8e6f501b255f8614c8fb7
Biswajit9999 <2003biswajit@gmail.com>
2024-06-25 14:33:13 +0530

    AdmissionData modified

--- src/components/elements/patient details/AdmissionData.tsx
+++ src/components/elements/patient details/AdmissionData.tsx
@@ -5,42 +5,32 @@ const AdmissionData: React.FC = () => {
   return (
     <div>
       {/* given exact height and width as per my laptop but need o be fixed soon */}
-      <div className="flex h-[43rem] w-[73%] flex-col bg-pink-200">
-        <div className="flex h-[10%] w-full flex-row items-center space-x-[2rem] bg-white">
-          <span className="text-[20px] font-bold hover:border-b-[.3rem] hover:border-[#FFB800]">
-            General
-          </span>
-          <span className="text-[20px] font-bold hover:border-b-[.3rem] hover:border-[#FFB800]">
-            Diagnosis
-          </span>
-          <span className="text-[20px] font-bold hover:border-b-[.3rem] hover:border-[#FFB800]">
-            Admission Details
-          </span>
-        </div>
+      <div className="flex pt-[3%] flex-col font-sansation text-[17px]/[19.03px] space-y-[5%]">
+        
         <div className="flex h-[18%] w-full flex-row justify-between">
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Insurance Provider</span>
+          <div className="flex h-full w-[16%] text-balance flex-col space-y-3">
+            <span className="">Insurance Provider</span>
             <span className="text-[#003A47]">National Insurance Pvt. Ltd.</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Policy No.</span>
+          <div className="flex h-full w-[16%] break-words flex-col space-y-3">
+            <span className="">Policy No.</span>
             <span className="text-[#003A47]">NIC000123456789</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Blood Pressure</span>
+          <div className="flex h-full w-[16%] text-balance flex-col space-y-3">
+            <span className="">Blood Pressure</span>
             <span className="text-[#003A47]">140/85</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Referance</span>
+          <div className="flex h-full w-[16%] text-balance flex-col space-y-3">
+            <span className="">Referance</span>
             <span className="text-[#003A47]">behala Rural Hosp.</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Primary Physicial</span>
+          <div className="flex h-full w-[16%] text-balance flex-col space-y-3">
+            <span className="">Primary Physicial</span>
             <span className="text-[#003A47]">Dr. A Sanyal</span>
           </div>
         </div>
         <div className="flex h-[22%] w-full flex-col space-y-3">
-          <span className="text-[17px]">Medical History</span>
+          <span className="">Medical History</span>
           <div className="w-full pr-[2rem] text-[#003A47]">
             The patient reported using oral antibiotics and mucolytics with no
             significant relief. He reported having used illicit drugs
@@ -49,48 +39,48 @@ const AdmissionData: React.FC = () => {
           </div>
         </div>
         <div className="flex h-[16%] w-full flex-row justify-between">
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Admission ID</span>
+          <div className="flex h-full text-wrap flex-col space-y-3">
+            <span className="">Admission ID</span>
             <span className="text-[#003A47]">ADI23456</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Treatment ID</span>
+          <div className="flex h-full text-wrap flex-col space-y-3">
+            <span className="">Treatment ID</span>
             <span className="text-[#003A47]">TDI23456</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Medication ID</span>
+          <div className="flex h-full text-wrap flex-col space-y-3">
+            <span className="">Medication ID</span>
             <span className="text-[#003A47]">MDI223456</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Date of admission</span>
+          <div className="flex h-full text-wrap flex-col space-y-3">
+            <span className="">Date of admission</span>
             <span className="flex flex-row justify-between text-[#003A47]">
               <span>24-06-2024</span>
               <FaPencil />
             </span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Date of discharge</span>
-            <span className="flex flex-row space-x-[7rem] text-[#003A47]">
-              <span>--</span>
+          <div className="flex h-full w-[16%] flex-col  space-y-3">
+            <span className="">Date of discharge</span>
+            <span className="flex flex-row justify-between space-x- text-[#003A47]">
+              <span>-</span>
               <FaPencil />
             </span>
           </div>
         </div>
-        <div className="flex h-[18%] w-full flex-row space-x-[3rem]">
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Ward</span>
+        <div className="flex h-[18%] w-full flex-row justify-between">
+          <div className="flex h-full flex-col space-y-3">
+            <span className="">Ward</span>
             <span className="text-[#003A47]">General Ward</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Room No.</span>
+          <div className="flex h-full flex-col space-y-3">
+            <span className="">Room No.</span>
             <span className="text-[#003A47]">101</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Attending Physicial</span>
+          <div className="flex h-full flex-col space-y-3">
+            <span className="text-[#2E2E2E]">Attending Physicial</span>
             <span className="text-[#003A47]">Dr. AKM. Sejedur Rahman</span>
           </div>
-          <div className="flex h-full w-[16%] flex-col space-y-3">
-            <span className="text-[17px]">Admission Type</span>
+          <div className="flex h-full flex-col space-y-3">
+            <span className="">Admission Type</span>
             <span className="text-[#003A47]">Referrall</span>
           </div>
         </div>
