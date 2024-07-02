687619bb6db3bcaec5636b2352dc8c81317e0076
Biswajit9999 <2003biswajit@gmail.com>
2024-07-02 21:26:42 +0530

    ...

--- src/components/elements/Depertment.tsx
+++ src/components/elements/Depertment.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Depertment = () => {
+const Depertment:React.FC = () => {
   return (
-    <div>
-      
+    <div className='w-full h-full flex flex-row'>
+      <div className='w-[60%] bg-black h-full'></div>
+      <div className='w-[40%] bg-zinc-600 h-full'></div>
     </div>
   )
 }
--- src/pages/hr-management.tsx
+++ src/pages/hr-management.tsx
@@ -1,44 +1,40 @@
-import { headers } from 'next/headers'
-import React from 'react'
-import { data, width } from 'tailwindcss/defaultTheme'
-import { DashboardTemplate } from '~/components'
-import Depertment from '~/components/elements/Depertment'
-import Designation from '~/components/elements/Designation'
-import ManageTile from '~/components/elements/ManageTile'
-import AddDoctorForm from '~/components/form/AddDoctorForm'
-import TileLayout from '~/components/layout/TileLayout'
-import CustomTable from '~/components/tables/CustomTable'
+import { headers } from "next/headers";
+import React from "react";
+import { data, width } from "tailwindcss/defaultTheme";
+import { DashboardTemplate } from "~/components";
+import Depertment from "~/components/elements/Depertment";
+import Designation from "~/components/elements/Designation";
+import ManageTile from "~/components/elements/ManageTile";
+import AddDoctorForm from "~/components/form/AddDoctorForm";
+import TileLayout from "~/components/layout/TileLayout";
+import CustomTable from "~/components/tables/CustomTable";
 
-const HrManagement:React.FC = () => {
+const HrManagement: React.FC = () => {
   return (
     <DashboardTemplate active_tile="Doctors">
-      <div className="h-full w-full p-[1%]">
+      <div className="h-full w-full flex flex-col p-[1%]">
         <div>
-          <div>
-            <ManageTile
-              title="Manage Doctor"
-              doctors={10}
-              patients={50}
-              nurses={20}
-              pharmacists={5}
-              laboratorists={3}
-              accountants={2}
-            />
-          </div>
-          <div className="h-full w-full">
-            <div className="w-full">
-              <TileLayout
-                tileone={"Depertment"}
-                tiletwo={"Designation"}
-                children1={<Depertment/>}
-                children2={<Designation/>}
-              />
-            </div>
-          </div>
+          <ManageTile
+            title="Manage HR"
+            doctors={10}
+            patients={50}
+            nurses={20}
+            pharmacists={5}
+            laboratorists={3}
+            accountants={2}
+          />
+        </div>
+        <div className="h-full w-full">
+          <TileLayout
+            tileone={"Depertment"}
+            tiletwo={"Designation"}
+            children1={<Depertment />}
+            children2={<Designation />}
+          />
         </div>
       </div>
     </DashboardTemplate>
-  )
-}
+  );
+};
 
-export default HrManagement
+export default HrManagement;
